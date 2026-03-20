ff92b448fe4a489d95f6a4cbf063fae7bbb8ddad
Mayank Kumar <mayankkumar270607@gmail.com>
2026-03-20 08:52:26 +0530

    Add solution for 1703B

--- 1703B_ICPC_Balloons/Solution.java
+++ 1703B_ICPC_Balloons/Solution.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
-import java.util.Arrays;
-
+import java.util.Set;
+import java.util.HashSet;
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -8,16 +8,9 @@ public class Solution {
         while (t-- > 0) {
             int n = sc.nextInt();
             String s = sc.next();
-            char[] a = s.toCharArray();
-            Arrays.sort(a);
-            int r = 0;
-            for (int i = 0;i<n;i++){
-                char c = a[i];
-                r +=2;
-                while (a[i]==c){
-                    r++;
-                }
-            }
+            Set<String> set = new HashSet<>();
+            for (char c : s.toCharArray()) set.add(String.valueOf(c));
+            System.out.println(n + set.size());
         }
         sc.close();
     }
