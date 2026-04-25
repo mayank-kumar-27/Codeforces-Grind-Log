b3139914c5f48dfda5c9e7197b314739c370a616
Mayank Kumar <mayankkumar270607@gmail.com>
2026-04-25 09:24:31 +0530

    Add solution for 1294A

--- 1294A_Collecting_Coins/solution.java
+++ 1294A_Collecting_Coins/solution.java
@@ -4,9 +4,12 @@ public class solution {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         while (t-- > 0) {
-            int s = 0;
-            for (int i = 0; i < 4; i++) s += sc.nextInt();
-            System.out.println(s % 3 == 0 ? "YES" : "NO");
+            int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt(), n = sc.nextInt();
+            int sum = a + b + c + n;
+            if (sum % 3 != 0) { System.out.println("NO"); continue; }
+            int target = sum / 3, max = Math.max(a, Math.max(b, c));
+            if (max <= target) System.out.println("YES");
+            else System.out.println("NO");
         }
         sc.close();
     }
