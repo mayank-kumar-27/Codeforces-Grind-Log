b65d9a17fb07dcdbf99974244b35bf4778273cf5
Mayank Kumar <mayankkumar270607@gmail.com>
2026-05-02 09:43:15 +0530

    Add solution for 1807B

---
+++ 1807B_Grab_the_Candies/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+            int e = 0, o = 0;
+            for (int num : arr) if (num % 2 == 0) e += num; else o += num;
+            System.out.println(e > o ? "YES" : "NO");
+        }
+        sc.close();
+    }
+}
