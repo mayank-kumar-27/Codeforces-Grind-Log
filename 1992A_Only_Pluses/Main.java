e11d095c3d6d580df84694961035468bebfa0d17
Mayank Kumar <mayankkumar270607@gmail.com>
2026-05-03 12:45:00 +0530

    Add solution for 1992A

---
+++ 1992A_Only_Pluses/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+            for (int i = 0; i < 5; i++) if (a <= b && a <= c) a++; else if (b <= a && b <= c) b++; else c++;
+            System.out.println(1L * a * b * c);
+        }
+        sc.close();
+    }
+    
+}
