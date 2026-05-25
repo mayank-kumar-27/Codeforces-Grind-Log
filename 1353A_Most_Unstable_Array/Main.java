b864335eeea1a0a6079b1c5d49464e8fc97b3c0a
Mayank Kumar <mayankkumar270607@gmail.com>
2026-05-25 20:10:43 +0530

    Add solution for 1353A

---
+++ 1353A_Most_Unstable_Array/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt(), m = sc.nextInt();
+            if (n == 1) System.out.println(0);
+            else if (n == 2) System.out.println(m);
+            else System.out.println(2 * m);
+        }
+        sc.close();
+    }
+}
