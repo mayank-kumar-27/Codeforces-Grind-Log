d07c4359bf22198fb29d9c6450633903b674f94e
Mayank Kumar <mayankkumar270607@gmail.com>
2026-05-27 13:01:14 +0530

    Add solution for 1806A

--- 1806A_Walking_Master/Main.java
+++ 1806A_Walking_Master/Main.java
@@ -6,7 +6,8 @@ public class Main {
         while (t-- > 0) {
             long a = sc.nextLong(), b = sc.nextLong(), c = sc.nextLong(), d = sc.nextLong();
             long u = d - b;
-            System.out.println(u < 0 || a + u > c ? -1 : u + c - (a + u));
+            if (u < 0 || a + u < c) System.out.println(-1);
+            else System.out.println(u + (a + u - c));
         }
         sc.close();
     }
