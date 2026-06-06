3dfa9d63cb51cdca6a6d811200a09857c9ad141b
Mayank Kumar <mayankkumar270607@gmail.com>
2026-06-06 14:16:30 +0530

    Add solution for 1358A

---
+++ 1358A_Park_Lighting/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            long n = sc.nextLong(), m = sc.nextLong();
+            System.out.println((n * m + 1) / 2);
+        }
+        sc.close();
+    }
+}
